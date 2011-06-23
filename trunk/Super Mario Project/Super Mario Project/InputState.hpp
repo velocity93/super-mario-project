0e497ff53221ca28768bbeb78a4354f9138af02c
openmengine <openmengine@58453a5e-5cee-f8df-839c-855c884778fb>
2011-06-23 15:33:34 +0000

    Ajout d'une classe InputState pour connaitre l'état des touches du clavier, et des boutons de la souris (par la suite). Ajout des ressources associées aux projectiles.

---
+++ trunk/Super Mario Project/Super Mario Project/InputState.hpp
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////
+// Keystate.hpp
+// Super Mario Project
+// Copyright (C) 2011  
+// Lionel Joseph lionel.r.joseph@gmail.com
+// Olivier Guittonneau openmengine@gmail.com
+////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#ifndef HPP_INPUTSTATE
+#define HPP_INPUTSTATE
+
+#include <vector>
+#include <SFML/Window.hpp>
+
+namespace SuperMarioProject
+{
+    enum KeyState {
+        KEY_STATE_RELEASED,
+        KEY_STATE_PRESSED,
+        KEY_STATE_JUST_PRESSED
+    };
+
+
+    enum Key {
+        KEY_NONE = -1,
+        KEY_BACKWARD,
+        KEY_FORWARD,
+        KEY_UP,
+        KEY_DOWN,
+        KEY_JUMP,
+        KEY_RUN,
+        KEY_ESCAPE,
+
+        KEY_NB_KEYS
+    };
+
+
+    class InputState
+    {
+    public:
+        InputState(const sf::Window *window);
+        void update();
+        KeyState operator[](Key k);
+
+    private:
+        const sf::Window *_window;
+        std::vector<KeyState> _states;
+        std::vector<sf::Key::Code> _sfmlKeys;
+    };
+} // namespace
+
+#endif // HPP_INPUTSTATE
