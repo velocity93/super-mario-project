53a21770d7c125f4e2482750cb7384180c3df50b
lionel.r.joseph@gmail.com <lionel.r.joseph@gmail.com@58453a5e-5cee-f8df-839c-855c884778fb>
2011-10-11 20:47:49 +0000

    Gestion des balises Tileset, Block et occ_blocks.

--- Super Mario Project/Super Mario Project/XMLParser.cpp
+++ Super Mario Project/Super Mario Project/XMLParser.cpp
@@ -72,26 +72,27 @@ namespace SuperMarioProject
 	int id_item = 0;
 	void item_tag(Level * level, const char **attrs)
 	{
-		id_item = 0;
 		level->addItem(new Item(attrs[1], (Item::Type)atoi(attrs[3])));
+		id_item++;
 	}
 
 	void occ_item_tag(Level * level, const char **attrs)
 	{
 		Vector2f position(atoi(attrs[1]), atoi(strchr(attrs[1], ':') + 1));
-		level->getItems()[id_item]->addNewItemOccurrence(position);
+		level->getItems()[id_item - 1]->addNewItemOccurrence(position);
 	}
 
 	int id_monster = 0;
 	void monster_tag(Level * level, const char **attrs)
 	{
 		level->addMonster(new Monster(attrs[1]));
+		id_monster++;
 	}
 
 	void occ_monster_tag(Level * level, const char **attrs)
 	{
 		Vector2f position(atoi(attrs[1]), atoi(strchr(attrs[1], ':') + 1));
-		level->getMonsters()[id_monster]->addNewMonsterOccurrence(position);
+		level->getMonsters()[id_monster - 1]->addNewMonsterOccurrence(position);
 	}
 
 	void pipe_tag(Level * level, const char **attrs)
@@ -108,22 +109,29 @@ namespace SuperMarioProject
 			level->getMonsters()[atoi(attrs[15])]));
 	}
 
+	int id_tileset = 0;
 	// <tileset img="nomTexture.png" size="x:y">
 	void tileset_tag(Level * level, const char **attrs)
 	{
 		Tileset* tileset = new Tileset(attrs[1]);
 		level->addTileset(tileset);
 		level->setBlockSize(atoi(attrs[3]), atoi(strchr(attrs[3], ':') + 1));
+		id_tileset++;
 	}
 
+	// <block physIndex="" />
 	void blocks_tag(Level * level, const char **attrs)
 	{
-		// TO DO
+		Block* block = new Block(level->getTilesets()[id_tileset - 1], atoi(attrs[1]));
+		level->addBlock(block);
 	}
 
+	//<occ_block actualModel="" alternativeModel="" pos="x:y"/>
 	void occ_blocks_tag(Level* level, const char** attrs)
 	{
-		// TO DO
+		Vector2f position(atoi(attrs[5]), atoi(strchr(attrs[5], ':') + 1));
+		Block* model = level->getBlock()[atoi(attrs[1])];
+		model->addNewBlockOccurrence(level->getBlock()[atoi(attrs[3])], position);
 	}
 
 	void error(void * ctx, const char * msg, ...)
@@ -149,12 +157,7 @@ namespace SuperMarioProject
 			BAD_CAST"pipe",
 			BAD_CAST"tileset",
 			BAD_CAST"blocks", 
-			BAD_CAST"occ_blocks", 
-			/*BAD_CAST"textures", 
-			BAD_CAST"bloc", 
-			BAD_CAST"layers", 
-			BAD_CAST"layer",
-			BAD_CAST"occ_block"*/
+			BAD_CAST"occ_blocks"
 		};
 		static const balise_func functions[] = {
 			level_tag,
@@ -173,15 +176,10 @@ namespace SuperMarioProject
 			tileset_tag,
 			blocks_tag,
 			occ_blocks_tag
-			/*balise_textures,
-			balise_bloc,
-			balise_layers,
-			balise_layer,
-			balise_occ_block*/
 		};
 		int i;
 
-		for(i = 0; i < 13; i++)
+		for(i = 0; i < 16; i++)
 		{
 			if(!xmlStrcmp(name, elements[i]))
 			{
@@ -199,6 +197,9 @@ namespace SuperMarioProject
 
 		if(xmlSAXUserParseFile(&sh, level, fileName.c_str()))
 			puts("Error in reading XML level file");
+
+		id_monster = 0;
+		id_tileset = 0;
 	}
 
 	void start_world_element(void *user_data, const xmlChar *name, const xmlChar **attrs) 
