8164afe79fd0c3706ddc159580e2f0413016d973
lionel.r.joseph@gmail.com <lionel.r.joseph@gmail.com@58453a5e-5cee-f8df-839c-855c884778fb>
2011-10-23 16:15:36 +0000

    Surcharge de la méthode Render de la classe Sprite de la SFML

---
+++ trunk/Super Mario Project/Super Mario Project/ReversedSprite.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////
+// ReversedSprite.cpp
+// Super Mario Project
+// Copyright (C) 2011  
+// Lionel Joseph lionel.r.joseph@gmail.com
+// Olivier Guittonneau openmengine@gmail.com
+////////////////////////////////////////////////////////////////////////
+
+#include "ReversedSprite.hpp"
+
+namespace Rendering 
+{
+	void Render(sf::RenderTarget& Target)
+	{
+
+	}
+}
