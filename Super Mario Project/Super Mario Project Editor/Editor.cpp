2336246b31fd4731277bdf60c9c9d2f81ded6fa1
lionel.r.joseph@gmail.com <lionel.r.joseph@gmail.com@58453a5e-5cee-f8df-839c-855c884778fb>
2012-03-30 18:07:18 +0000

    Ajout d'un projet Qt pour l'éditeur de niveau

---
+++ Super Mario Project/Super Mario Project Editor/Editor.cpp
@@ -0,0 +1,12 @@
+#include "Editor.hpp"
+
+Editor::Editor(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
+{
+	ui.setupUi(this);
+}
+
+Editor::~Editor()
+{
+
+}
---
+++ Super Mario Project/Super Mario Project Editor/main.cpp
@@ -0,0 +1,10 @@
+#include "Editor.hpp"
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	Editor w;
+	w.show();
+	return a.exec();
+}
